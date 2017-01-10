7f5b26d8af6472f8ca1baa72a758cd04d65e0abb
Carlotte Girardot <charlotte.girardot@ensimag.grenoble-inp.fr>
2017-01-10 17:47:54 +0100

    Bon, après c'est fini avec writeoff

--- main_lissage.m
+++ main_lissage.m
@@ -4,7 +4,6 @@ clearvars;
 sigma_c = 1;
 sigma_s = 1;
 nb_iter = 1;
-path = '../Data/';
 %-------- Constantes ----------------
 sigma_c_2 = sigma_c * sigma_c;
 sigma_s_2 = sigma_s * sigma_s;
@@ -12,10 +11,8 @@ ro = 2 * sigma_c;
 
 %-------- Beginning -----------------
 %Récupération du nom du fichier 
-prompt = 'Nom du fichier = ';
-str = input(prompt, 's');
+filename = input('Nom du fichier en entrée', 's');
 %str = 'bear.off'
-filename = strcat(path, str);
 
 %Lecture du fichier
 [vertices, faces] = read_off(filename);
@@ -26,6 +23,5 @@ filename = strcat(path, str);
   end
  
   %récupération nom fichier 
-  prompt = 'Nom du fichier de sortie ? ';
-  strout = input(prompt, 's');
+  strout = input('Nom du fichier de sortie ?', 's');
   write_off(strout,vertices,faces);
