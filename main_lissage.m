928d0fcdec2d80b9e7749b41e7028d1fe86a60a1
Carlotte Girardot <charlotte.girardot@ensimag.grenoble-inp.fr>
2017-01-10 15:41:42 +0100

    Petites modifications du main

--- main_lissage.m
+++ main_lissage.m
@@ -1,12 +1,19 @@
-filename = 'Data/bear.off';
-
-
-[vertices, faces] = read_off(filename);
-
+%filename = 'Data/bear.off';
+%-------- Constantes ----------------
 sigma_c_2 = 1;
 sigma_s_2 = 1;
-ro = 1;
+ro = 2 * sigma_c;
 nb_iter = 1;
+path = '../Data/';
+
+%-------- Beginning -----------------
+%Récupération du nom du fichier 
+prompt = 'Nom du fichier = ';
+str = input(prompt, 's');
+filename = strcat(path, str);
+
+%Lecture du fichier
+[vertices, faces] = read_off(filename);
 
 %scatter3(vertices(:,1),vertices(:,2),vertices(:,3))
 meshgrid(vertices(:,1),vertices(:,2),vertices(:,3))
