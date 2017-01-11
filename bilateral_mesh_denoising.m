33fc76ce8a21e25a7fd4ed789c09f0a088f7eb3c
Carlotte Girardot <charlotte.girardot@ensimag.grenoble-inp.fr>
2017-01-11 17:36:30 +0100

    .

--- bilateral_mesh_denoising.m
+++ bilateral_mesh_denoising.m
@@ -6,18 +6,16 @@ function [ vertices_denoised ] = bilateral_mesh_denoising( vertices,faces,sigma_
 normals = compute_normal(vertices,faces);
 vertices_denoised = zeros(size(vertices, 1),3);
 %---fast neighborhood computing
-HT = repartition(vertices, 2*sqrt(sigma_c_2));
+%HT = repartition(vertices, 2*sqrt(sigma_c_2));
 
 for k=1:size(vertices, 1)
     
     %---Naive neighborhood computing
-    %neighborhood = compute_neighborhood(vertices,k,ro);
+    neighborhood = compute_neighborhood(vertices,k,ro);
     
     %---Fast neighborhood computing
-    
-    
     [ minX, minY, minZ, maxX, maxY, maxZ ] = bornesvertices( vertices );
-   neighborhood = fast_compute_neighborhood( HT, vertices, 2*sqrt(sigma_c_2), k, minX, minY, minZ);
+    %neighborhood = fast_compute_neighborhood( HT, vertices, 2*sqrt(sigma_c_2), k, minX, minY, minZ);
     
     % A voire comment faire
     vertices_denoised(k,:) = denoisePoint( k, normals(k,:), vertices, sigma_c_2, sigma_s_2, neighborhood);
--- main_lissage.m
+++ main_lissage.m
@@ -1,27 +1,24 @@
 clearvars;
-
+%____________Récupération du nom du fichier_________
+filename = input('Nom du fichier en entrée : ', 's');
+%str = 'bear.off'
+%____________Initialisation___________________
+%Lecture du fichier
+[vertices, faces] = read_off(filename);
+[ minX, minY, minZ, maxX, maxY, maxZ ] = bornesvertices( vertices );
 %-------- Paramètres ---------------
-sigma_c = 1;
-sigma_s = 1;
+sigma_c = 0.05*(maxX + maxY + maxZ - minX - minY - minZ);
+sigma_s = 2*sigma_c;
 nb_iter = 1;
 %-------- Constantes ----------------
 sigma_c_2 = sigma_c * sigma_c;
 sigma_s_2 = sigma_s * sigma_s;
 ro = 2 * sigma_c;
 
-%-------- Beginning -----------------
-%Récupération du nom du fichier 
-filename = input('Nom du fichier en entrée', 's');
-%str = 'bear.off'
-
-%Lecture du fichier
-[vertices, faces] = read_off(filename);
-
-
   for i=1:nb_iter
       vertices = bilateral_mesh_denoising( vertices, faces, sigma_c_2,sigma_s_2,ro);
   end
  
   %récupération nom fichier 
-  strout = input('Nom du fichier de sortie ?', 's');
+  strout = input('Nom du fichier de sortie : ', 's');
   write_off(strout,vertices,faces);
